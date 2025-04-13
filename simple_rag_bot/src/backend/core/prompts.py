388250946186de4d0ec9d42cbd7df3adcbb4e0be
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2025-04-13 18:13:10 +0530

    fix: redundancy in prompt variables

--- simple_rag_bot/src/backend/core/prompts.py
+++ simple_rag_bot/src/backend/core/prompts.py
@@ -4,7 +4,7 @@ router_template = """You are a helpful assitant whose purpose is to help users w
 Given user question and previous conversation history, utlize the appropriate tools where required to help them.
 Understand the user question and identify which tools are more suitable to help them and use it accordingly.
 
-User Question: {user_query}
+Conversation history:
 """
 router_prompt = ChatPromptTemplate.from_messages([
     ("system", router_template),
@@ -14,13 +14,11 @@ router_prompt = ChatPromptTemplate.from_messages([
 
 rag_template = """You are a friendly and helpful chat bot. Your role is to help users by answering questions based on the sources provided.
 Maintain a friendly and supportive tone while providing accurate and helpful responses. Utilize the provided conversation history to understand previous interactions.
-Avoid adding any other explanation.
+Based on the sources generate a clear answer to user's question. Avoid adding any other explanation.
+
 Sources: {sources}
 
-Conversation History: {chat_history}
-
-User Query: {user_query}
-Response:
+Conversation History:
 """
 rag_prompt = ChatPromptTemplate.from_messages([
     ("system", rag_template),
@@ -28,17 +26,31 @@ rag_prompt = ChatPromptTemplate.from_messages([
     ("user", "{user_query}"),
 ])
 
+chitchat_template = """You are a friendly and helpful chat bot named ash. Your role is to help users by handling simple chitchat like hi, bye, thanks etc.
+Maintain a friendly and supportive tone while providing accurate and helpful responses. Utilize the provided conversation history to understand previous interactions.
+Respond appropriately to the user message, and always assure them that you are here to help.
+
+Conversation History:
+"""
+chitchat_prompt = ChatPromptTemplate.from_messages([
+    ("system", chitchat_template),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{user_query}"),
+])
+
+# TODO: Improve the rewrite prompt, make it concise
 query_rewrite_template = """Given the following conversation history and user followup question, rephrase the user query to be a standalone question.
 The rephrased question should be understood without the chat history and should be meaningful and complete on its own.
-If the query is independent and is not related to the chat history, then return it as is. In any other case, rewrite the query.
+Make sure that the rephrased question has all the details to be complete on it's own.
 
-Conversation History: {chat_history}
-User Query: {question}
+If the query is independent and is not related to the chat history, then return it as is without modifications. In any other case, rephrase the query.
+If the followup is not a question, but a statement, then return it as is without modifications.
+Avoid generating a response to the followup, your role is only to rephrase the input based on history.
 
-Standalone Question:
+Chat history:
 """
 query_rewrite_prompt = ChatPromptTemplate.from_messages([
     ("system", query_rewrite_template),
     MessagesPlaceholder(variable_name="chat_history"),
-    ("user", "{question}"),
+    ("user", "Followup message: {question}. Rephrased version:"),
 ])
